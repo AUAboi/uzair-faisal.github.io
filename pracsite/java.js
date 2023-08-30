749e5729526cc9294a9bda4657a3af3601837a15
Uzair-faisal <143415368+Uzair-faisal@users.noreply.github.com>
2023-08-30 19:48:18 +0500

    change

--- pracsite/java.js
+++ pracsite/java.js
@@ -10,45 +10,66 @@ let icon2 = document.querySelector("#ic2");
 let icon3 = document.querySelector("#ic3");
 let icon4 = document.querySelector("#ic4");
 let a = 0;
+let b = 0;
 
-function i1() {
-  setTimeout(ic1, 1000);
+window.addEventListener("resize", checker);
+
+a = 0;
+function checker() {
+  console.log(window.innerWidth);
+  a++;
+  if (window.innerWidth > 950) {
+    location.reload();
+  } else if (window.innerWidth < 950 && a == 1) {
+    gh();
+    console.log(a);
+  }
 }
 
-function i2() {
-  setTimeout(ic2, 1000);
+if (a == 0 && window.innerWidth < 950) {
+  a++;
+  gh();
 }
 
-function i3() {
-  setTimeout(ic3, 1000);
-}
+function gh() {
+  function i1() {
+    setTimeout(ic1, 1000);
+  }
 
-function i4() {
-  setTimeout(ic4, 1000);
-}
+  function i2() {
+    setTimeout(ic2, 1000);
+  }
 
-function ic1() {
-  icon4.style.display = "none";
-  icon1.style.display = "block";
-  setTimeout(i2, 5000);
-}
+  function i3() {
+    setTimeout(ic3, 1000);
+  }
 
-function ic2() {
-  icon1.style.display = "none";
-  icon2.style.display = "block";
-  setTimeout(i3, 5000);
-}
+  function i4() {
+    setTimeout(ic4, 1000);
+  }
 
-function ic3() {
-  icon2.style.display = "none";
-  icon3.style.display = "block";
-  setTimeout(i4, 5000);
-}
+  function ic1() {
+    icon4.style.display = "none";
+    icon1.style.display = "block";
+    setTimeout(i2, 5000);
+  }
 
-function ic4() {
-  icon3.style.display = "none";
-  icon4.style.display = "block";
-  setTimeout(i1, 5000);
-}
+  function ic2() {
+    icon1.style.display = "none";
+    icon2.style.display = "block";
+    setTimeout(i3, 5000);
+  }
 
-i1();
+  function ic3() {
+    icon2.style.display = "none";
+    icon3.style.display = "block";
+    setTimeout(i4, 5000);
+  }
+
+  function ic4() {
+    icon3.style.display = "none";
+    icon4.style.display = "block";
+    setTimeout(i1, 5000);
+  }
+  i1();
+}
