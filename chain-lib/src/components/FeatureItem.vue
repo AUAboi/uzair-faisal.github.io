f7149be3468c3f6f57c9d07ea93f32eb0ab197d3
Uzair-faisal <143415368+Uzair-faisal@users.noreply.github.com>
2023-09-05 14:26:27 +0500

    added vue support

---
+++ chain-lib/src/components/FeatureItem.vue
@@ -0,0 +1,16 @@
+<script setup>
+defineProps(["feature"])
+</script>
+
+<template>
+    <div class="mid-everything" id="ic1">
+        <div class="img-cont">
+            <div class="icon-bg"></div>
+            <div v-html="feature.svg"></div>
+        </div>
+        <h4>{{ feature.title }}</h4>
+        <p>
+            {{ feature.description }}
+        </p>
+    </div>
+</template>
---
+++ chain-lib/src/components/Header.vue
@@ -0,0 +1,28 @@
+<script setup>
+defineProps({
+    user:{
+        required: true,
+        type: String
+    }
+})
+</script>
+
+<template>
+    <header>
+        <nav>
+            <h1>{{ user }}</h1>
+            <ul>
+                <li>Home</li>
+                <li>Home</li>
+                <li>Home</li>
+                <li>Home</li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<style scoped>
+h1 {
+    color: red;
+}
+</style>
