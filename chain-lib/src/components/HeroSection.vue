c6156806e2bcc3a3ae308a87e7e0fd7e6e3e6c94
auaboi <61789531+AUAboi@users.noreply.github.com>
2023-09-12 18:16:46 +0500

    composition changes

---
+++ chain-lib/src/components/HeroSection.vue
@@ -0,0 +1,74 @@
+<template>
+  <section class="container abc">
+    <div class="div-mid-left">
+      <div id="bigtextdiv">
+        <div id="wasted" class="wastedd">
+          <h1 id="bigText" class="waster">
+            Like web3 but <span id="span1">multichain</span>
+          </h1>
+
+          <div>
+            <h2 id="waste1" class="waster">Build your crypto app in seconds</h2>
+          </div>
+          <div>
+            <p id="waste2" class="waster">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit
+              doloremque neque amet illum mollitia corporis aliquam dolores fuga
+              consequatur, deserunt, consequuntur repellat facere a tenetur eius
+              dolore enim, praesentium sed.
+            </p>
+          </div>
+          <div id="button-cont">
+            <button id="b2">
+              <span id="button-text">Get Started</span>
+              <span class="arrow-container">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                >
+                  <g clip-path="url(#clip0_1_45)">
+                    <path
+                      d="M16.004 9.414L7.39703 18.021L5.98303 16.607L14.589 8H7.00403V6H18.004V17H16.004V9.414Z"
+                      fill="white"
+                    />
+                  </g>
+                  <defs>
+                    <clipPath id="clip0_1_45">
+                      <rect width="24" height="24" fill="white" />
+                    </clipPath>
+                  </defs>
+                </svg>
+                <span class="arrow-bg"></span>
+              </span>
+            </button>
+          </div>
+
+          <div>
+            <p id="waste3" class="waster">Our Partners</p>
+            <div class="ullis">
+              <div>
+                <img src="assets/chch1.png" />
+              </div>
+              <div>
+                <img src="assets/chch2.png" />
+              </div>
+              <div>
+                <img src="assets/chch3.png" />
+              </div>
+              <div>
+                <img src="assets/chch4.png" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div id="img-cont1">
+      <img src="assets/Group 2483.png" />
+    </div>
+  </section>
+</template>
